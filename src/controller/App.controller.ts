24c5a4988741c301f7142ac3ea3dae54aa34a165
Peter Muessig <peter.muessig@sap.com>
2022-03-10 00:01:29 +0100

    Initial commit

---
+++ src/controller/App.controller.ts
@@ -0,0 +1,13 @@
+import BaseController from "./BaseController";
+
+/**
+ * @namespace com.myorg.myapp.controller
+ */
+export default class App extends BaseController {
+
+	public onInit() : void {
+		// apply content density mode to root view
+		this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+	}
+
+}
